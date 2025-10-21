94b14dcb49df0a9edc9226e3944b43f54b154879
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-10-21 14:12:08 +0200

    Created car analytics project and added car shared project reference to it

---
+++ Car.Analytics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Car.Analytics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
